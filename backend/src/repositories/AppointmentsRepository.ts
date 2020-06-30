5aca741786ab67d648a88908aaf5280392367a3f
João Giovanni <joao.oliveira@maltacobrancas.com.br>
2020-06-30 17:17:25 -0300

    Appointment model and repository

---
+++ backend/src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,31 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  /**
+   * Find a booked appointment to the same date
+   */
+  public findByDate(date: Date): Appointment | null {
+    const found = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+    return found || null;
+  }
+
+  /**
+   * Create a new appointment
+   */
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+    this.appointments.push(appointment);
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
