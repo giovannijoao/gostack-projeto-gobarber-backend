e02bbfb416bce9160600b809aadc263740ce3886
João Giovanni <joao.oliveira@maltacobrancas.com.br>
2020-06-30 17:01:39 -0300

    Validating an appointment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => res.json({
   message: 'Hello World',
 }));
+
+app.use('/', routes);
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
