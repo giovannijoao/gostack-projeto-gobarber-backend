e1cbfce292566c891efedc913617e9e17145dab8
João Giovanni <joao.oliveira@maltacobrancas.com.br>
2020-07-01 15:38:38 -0300

    Handling user avatar upload

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
-
+const upload = multer(uploadConfig);
 usersRouter.post('/', async (request, response) => {
   try {
     const { name, email, password } = request.body;
@@ -20,5 +23,21 @@ usersRouter.post('/', async (request, response) => {
     });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+    try {
+      return response.json({});
+    } catch (err) {
+      return response.status(400).json({
+        message: err.message,
+      });
+    }
+  },
+);
 
 export default usersRouter;
