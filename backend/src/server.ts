c3d82a5f08cbcd9eeaefbb19b4486856f9fc09f0
João Giovanni <joao.oliveira@maltacobrancas.com.br>
2020-06-30 21:23:12 -0300

    TypeOrm Setup and Migrations

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import routes from './routes';
+import './database';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => res.json({
-  message: 'Hello World',
-}));
+app.get('/', (req, res) =>
+  res.json({
+    message: 'Hello World',
+  }),
+);
 
 app.use('/', routes);
 
