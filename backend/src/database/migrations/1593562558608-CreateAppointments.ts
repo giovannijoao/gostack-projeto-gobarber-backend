dd405351470a660e997531d56027bb9bff28d9a5
João Giovanni <joao.oliveira@maltacobrancas.com.br>
2020-06-30 22:34:33 -0300

    Relations in database

--- backend/src/database/migrations/1593562558608-CreateAppointments.ts
+++ backend/src/database/migrations/1593562558608-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1593562558608
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
