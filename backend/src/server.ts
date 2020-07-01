e2ffed215938d6b2306d2099bc9bac62b97db929
João Giovanni <joao.oliveira@maltacobrancas.com.br>
2020-07-01 16:10:31 -0300

    Route to serve static files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,11 +2,12 @@ import express from 'express';
 import routes from './routes';
 import 'reflect-metadata';
 import './database';
+import upload from './config/upload';
 
 const app = express();
 
 app.use(express.json());
-
+app.use('/files', express.static(upload.directory));
 app.get('/', (req, res) =>
   res.json({
     message: 'Hello World',
