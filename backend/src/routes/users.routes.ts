2923c27bd89ce45033f73262b1a4d76ba2778168
João Giovanni <joao.oliveira@maltacobrancas.com.br>
2020-07-01 16:08:37 -0300

    Avatar upload

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import CreateUserService from '../services/CreateUserService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import uploadConfig from '../config/upload';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -29,9 +30,14 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file);
     try {
-      return response.json({});
+      const updateUserAvatarService = new UpdateUserAvatarService();
+      const user = await updateUserAvatarService.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+      delete user.password;
+      return response.json(user);
     } catch (err) {
       return response.status(400).json({
         message: err.message,
