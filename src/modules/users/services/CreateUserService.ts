f93fdc6e009c790520cb1c1c672ba9a89ddf2709
João Giovanni <joao.oliveira@maltacobrancas.com.br>
2020-07-14 20:16:41 -0300

    Created FakeUserRepository and CreateUserService.spec.ts

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -18,10 +18,10 @@ class CreateUserService {
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
     const checkUserExists = await this.usersRepository.findByEmail(email);
-    const hashedPassword = await hash(password, 8);
     if (checkUserExists) {
       throw new AppError('Email address already used');
     }
+    const hashedPassword = await hash(password, 8);
     const user = await this.usersRepository.create({
       name,
       email,
