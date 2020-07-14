3baac7df1403bc47c8615ea6150c298d55cbc741
João Giovanni <joao.oliveira@maltacobrancas.com.br>
2020-07-13 22:16:28 -0300

    Created first test: CreateAppointmentService.spec.ts

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,22 @@
-test('Sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import 'reflect-metadata';
+import CreateAppointmentService from './CreateAppointmentService';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+    const appointment = await createAppointment.execute({
+      provider_id: '123',
+      date: new Date(),
+    });
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123');
+  });
+
+  // it('should not be able to create two appointments on the same time', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
 });
