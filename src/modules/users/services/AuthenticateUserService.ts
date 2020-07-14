11b49f2bf1b0727cd798dac6c791fe2fdd3e553b
João Giovanni <joao.oliveira@maltacobrancas.com.br>
2020-07-14 20:44:06 -0300

    Created HashProvider and AuthenticateUserService.spec.ts

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,10 +1,10 @@
-import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import auth from '@config/auth';
 import AppError from '@shared/errors/AppError';
 import { injectable, inject } from 'tsyringe';
 import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 const { secret, expiresIn } = auth.jwt;
 
@@ -21,6 +21,7 @@ interface IResponse {
 class AuthenticateUserService {
   constructor(
     @inject('UsersRepository') private usersRepository: IUsersRepository,
+    @inject('HashProvider') private hashProvider: IHashProvider,
   ) {}
 
   public async execute({ email, password }: IRequest): Promise<IResponse> {
@@ -28,7 +29,10 @@ class AuthenticateUserService {
     if (!user) {
       throw new AppError('Incorrect email/password combination', 401);
     }
-    const passwordMatched = await compare(password, user.password);
+    const passwordMatched = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
     if (!passwordMatched) {
       throw new AppError('Incorrect email/password combination', 401);
     }
