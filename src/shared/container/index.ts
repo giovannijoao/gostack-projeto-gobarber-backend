8ff1d480d359d384501ecc59c9dcf682564d9382
João Giovanni <joaogiovannivilela@gmail.com>
2020-07-28 21:06:29 -0300

    Created Routes, Controllers and a new migration

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,7 +5,7 @@ import '@modules/users/providers';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
-import UsersRepository from '@modules/users/infra/typeorm/repository/UsersRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
