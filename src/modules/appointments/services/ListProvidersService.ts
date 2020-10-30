502c348ff6aebbf53c802ad5637a89c53838263f
João Giovanni <joaogiovannivilela@gmail.com>
2020-10-30 09:16:59 -0300

    Applied cache in ListProvidersService

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,17 +1,27 @@
 import { injectable, inject } from 'tsyringe';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import User from '@modules/users/infra/typeorm/entities/User';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import cache from '@config/cache';
 
 @injectable()
 class ListProvidersService {
   constructor(
     @inject('UsersRepository') private usersRepository: IUsersRepository,
+    @inject('CacheProvider') private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute(user_id: string): Promise<User[]> {
-    return this.usersRepository.findAllUsers({
-      expect_user_id: user_id,
-    });
+    const cacheKey = `providers-list:${user_id}`;
+    let users = await this.cacheProvider.recover<User[]>(cacheKey);
+    if (!users) {
+      users = await this.usersRepository.findAllUsers({
+        expect_user_id: user_id,
+      });
+      console.log('Query realizada no banco');
+      await this.cacheProvider.save(cacheKey, users);
+    }
+    return users;
   }
 }
 
