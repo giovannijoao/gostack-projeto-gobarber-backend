fd82c78068ba960edf3cc420a439319d4fd64d46
João Giovanni <joaogiovannivilela@gmail.com>
2020-10-30 13:36:57 -0300

    Using RateLimiterFlexible

---
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,33 @@
+import cacheConfig from '@config/cache';
+import redis from 'redis';
+import { NextFunction, Request, Response } from 'express';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+import AppError from '@shared/errors/AppError';
+
+const { redis: redisConfig } = cacheConfig.config;
+const redisClient = redis.createClient({
+  host: redisConfig.host,
+  port: redisConfig.port,
+  password: redisConfig.password,
+  enable_offline_queue: false,
+});
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'middleware',
+  points: 5,
+  duration: 5,
+});
+
+export default async function rateLimiter(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    await limiter.consume(req.ip);
+    next();
+  } catch (error) {
+    throw new AppError('Too Many Requests', 429);
+  }
+}
