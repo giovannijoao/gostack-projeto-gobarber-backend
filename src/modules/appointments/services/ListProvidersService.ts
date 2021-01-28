4865993831c356cdcd1ca147757a9fbd40124ce0
João Giovanni <joaogiovannivilela@gmail.com>
2021-01-28 19:34:00 -0300

    Excluding password from list providers service response

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from 'tsyringe';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import User from '@modules/users/infra/typeorm/entities/User';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 
 @injectable()
 class ListProvidersService {
@@ -13,10 +14,12 @@ class ListProvidersService {
   public async execute(user_id: string): Promise<User[]> {
     const cacheKey = `providers-list:${user_id}`;
     let users = await this.cacheProvider.recover<User[]>(cacheKey);
+    users = null;
     if (!users) {
       users = await this.usersRepository.findAllUsers({
         expect_user_id: user_id,
       });
+      users = classToClass(users);
       await this.cacheProvider.save(cacheKey, users);
     }
     return users;
