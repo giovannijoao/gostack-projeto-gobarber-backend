945f7e7fc1d24e0194468c99262f61f0fe2db866
João Giovanni <joaogiovannivilela@gmail.com>
2021-02-08 20:18:32 -0300

    Fixed ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -1,3 +1,7 @@
+const rootDir = process.env.NODE_ENV === "development" || process.env.TS_NODE_DEV  ?
+  "src" :
+  "dist"
+
 module.exports = [{
   "name": "default",
   "type": "postgres",
@@ -7,13 +11,14 @@ module.exports = [{
   "password": process.env.APP_DATABASE_POSTGRES_PASSWORD || "docker",
   "database": process.env.APP_DATABASE_POSTGRES_DATABASE || "gostack_gobarber",
   "entities": [
-    "dist/modules/**/infra/typeorm/entities/*.js"
+    `./${rootDir}/modules/**/infra/typeorm/entities/*{.ts,.js}`
   ],
   "migrations": [
-    "./src/shared/infra/typeorm/migrations/*.ts"
+    `./${rootDir}/shared/infra/typeorm/migrations/*{.ts,.js}`
   ],
   "cli": {
-    "migrationsDir": "./src/shared/infra/typeorm/migrations"
+    "migrationsDir": "./src/shared/infra/typeorm/migrations",
+    "entitiesDir": "./src/modules/**/infra/typeorm/entities",
   }
 }, {
   "name": "mongo",
@@ -22,7 +27,7 @@ module.exports = [{
   "port": process.env.APP_DATABASE_MONGODB_PORT || 27017,
   "database": process.env.APP_DATABASE_MONGODB_DATABASE || "gostack_gobarber",
   "entities": [
-    "./src/modules/**/infra/typeorm/schemas/*.ts"
+    `./${rootDir}/modules/**/infra/typeorm/schemas/*{.ts,.js}`
   ],
   "useUnifiedTopology": true
 }]
